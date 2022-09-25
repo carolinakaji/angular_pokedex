ab23be95e2b259bd1633da9ae1e6c33f57c6eeb5
Carolina Kaji <carolina.kaji@gmail.com>
2022-09-25 17:31:51 -0300

    Adicionado o método para chamar os pokemons e adicionar o status no results

--- pokedex/src/app/service/poke-api.service.ts
+++ pokedex/src/app/service/poke-api.service.ts
@@ -1,14 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokeApiService {
 
-  private url: string = 'https://pokeapi.co/api/v2/ability/?limit=0&offset=100';
+  private url: string = 'https://pokeapi.co/api/v2/pokemon?offset=0&limit=100';
   constructor(
     private http: HttpClient
   ) { }
@@ -20,7 +20,21 @@ export class PokeApiService {
       tap(
         res => res
       ),
-      tap(res => console.log(res))
+      //O tap faz uma ação mas não retorna um valor obrigatóriamente, como o map
+      //Neste caso, queremos incluir o status em cada item dentro do results. O results é mapeado e chamado o método get para listar, a url é a resposta.url. Então é mapeado novamente e no subscribe, ele adiciona o status
+      tap(res => {
+        res.results.map((resPokemons: any) => {
+          this.apiGetPokemons(resPokemons.url).subscribe(
+            res => resPokemons.status = res
+          )
+        })
+      })
+    )
+  }
+
+  public apiGetPokemons(url: string): Observable<any> {
+    return this.http.get<any>(url).pipe(
+      map(res => res)
     )
   }
 }
