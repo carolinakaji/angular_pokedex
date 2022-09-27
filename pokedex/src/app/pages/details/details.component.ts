453f91aa671545d3160be3e18bc15afa68ec22c2
Carolina Kaji <carolina.kaji@gmail.com>
2022-09-27 13:26:22 -0300

    lógica para recuperar o id pelo acive route, get das apis de pokemon e especies

--- pokedex/src/app/pages/details/details.component.ts
+++ pokedex/src/app/pages/details/details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { forkJoin } from 'rxjs';
+import { PokeApiService } from 'src/app/service/poke-api.service';
 
 @Component({
   selector: 'app-details',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsComponent implements OnInit {
 
-  constructor() { }
+  private urlPokemon: string = 'https://pokeapi.co/api/v2/pokemon';
+  private urlName: string = 'https://pokeapi.co/api/v2/pokemon-species';
+
+  constructor(
+    private activeRoute: ActivatedRoute,
+    private pokeApiService: PokeApiService
+  ) { }
 
   ngOnInit(): void {
+    this.pokemon;
+  }
+
+  get pokemon(){
+    //O active route pega o valor do id que está na url da página
+    const id = this.activeRoute.snapshot.params['id'];
+    const pokemon = this.pokeApiService.apiGetPokemons(`${this.urlPokemon}/${id}`);
+    const name = this.pokeApiService.apiGetPokemons(`${this.urlName}/${id}`);
+
+    // Carrega as 2 apis ao mesmo tempo
+  return forkJoin([pokemon, name]).subscribe(
+      res => {
+        console.log(res);
+      }
+    )
+
+
   }
 
 }
