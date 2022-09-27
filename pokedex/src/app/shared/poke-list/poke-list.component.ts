3a0147322ba75fcc284687a21542b64e3a279d27
Carolina Kaji <carolina.kaji@gmail.com>
2022-09-26 22:05:00 -0300

    Implementando lógica do filtro

--- pokedex/src/app/shared/poke-list/poke-list.component.ts
+++ pokedex/src/app/shared/poke-list/poke-list.component.ts
@@ -9,15 +9,27 @@ import { PokeApiService } from 'src/app/service/poke-api.service';
 export class PokeListComponent implements OnInit {
 
   public getAllPokemons: any;
+  private setAllPokemons: any;
   constructor(
     private pokeApiService: PokeApiService
   ) { }
 
   ngOnInit(): void {
     this.pokeApiService.apiListAllPokemons.subscribe(res => {
-      this.getAllPokemons = res.results;
-      console.log(this.getAllPokemons)
+      this.setAllPokemons = res.results;
+      this.getAllPokemons = this.setAllPokemons;
+      // console.log(this.getAllPokemons)
     });
+  }
+
+  public getSearch(value: string){
+    const filter = this.setAllPokemons.filter((res: any) => {
+      return !res.name.indexOf(value.toLowerCase());
+    });
+
+    this.getAllPokemons = filter;
+
+    /*Para que o filtro funcione, selecionando os cards com os pokemons conforme digita o nome e quando apaga, voltam os cards, criou-se o setAllPokemons, desta forma, sendo uma variável privada, somente o componente tem acesso. E ele armazena o valor do results, não o getAllPokemons. O getAllPokemons receberá o que for filtrado, mas o valor nunca será sobrescrito */
   }
 
 }
