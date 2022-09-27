5629297002bbee25bf1065dc7c3af17954581198
Carolina Kaji <carolina.kaji@gmail.com>
2022-09-26 22:03:42 -0300

    Adicionado o event emitter

--- pokedex/src/app/shared/poke-search/poke-search.component.ts
+++ pokedex/src/app/shared/poke-search/poke-search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-poke-search',
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PokeSearchComponent implements OnInit {
 
+  @Output() public emmitSearch: EventEmitter<string> = new EventEmitter();
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  public search(value: string) {
+    this.emmitSearch.emit(value);
+  }
 }
